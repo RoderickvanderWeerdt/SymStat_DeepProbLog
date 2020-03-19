d5dd8a7967e02810c58738129567e4a666adf817
RoderickvanderWeerdt <rvanderweerdt@hotmail.com>
2020-03-19 11:37:15 +0100

    run.py now uses sudoku_mini.pl and the sudoku test set

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+#changes made to use the sudoku data instead of the addition data
+
 from train import train_model
 from data_loader import load
 from examples.NIPS.MNIST.mnist import test_MNIST, MNIST_Net, neural_predicate
@@ -7,11 +9,18 @@ from network import Network
 import torch
 
 
-queries = load('train_data.txt')
+train_queries = load('train_data.txt')
+test_queries = load('4x4_sudokus_5open.pl')[:100]
 
-with open('addition.pl') as f:
+
+def test(model):
+    acc = model.accuracy(test_queries, test=True)
+    print('Accuracy: ', acc)
+    return [('accuracy', acc)]
+
+
+with open('sudoku_mini.pl') as f:
     problog_string = f.read()
-
 
 network = MNIST_Net()
 net = Network(network, 'mnist_net', neural_predicate)
@@ -19,4 +28,5 @@ net.optimizer = torch.optim.Adam(network.parameters(),lr = 0.001)
 model = Model(problog_string, [net], caching=False)
 optimizer = Optimizer(model, 2)
 
-train_model(model,queries, 1, optimizer,test_iter=1000,test=test_MNIST,snapshot_iter=10000)
+test(model)
+train_model(model,train_queries, 1, optimizer,test_iter=1000,test=test_MNIST,snapshot_iter=10000)
