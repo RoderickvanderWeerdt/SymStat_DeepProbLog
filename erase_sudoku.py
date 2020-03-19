dba213b4ac311d355016035f9ff9c3bcb789327d
RoderickvanderWeerdt <rvanderweerdt@hotmail.com>
2020-03-19 10:27:07 +0100

    now uses mnist images from the testset instead of numbers in the sudokus

--- erase_sudoku.py
+++ erase_sudoku.py
@@ -1,9 +1,19 @@
+import torchvision
 import random
 
+testset = torchvision.datasets.MNIST(root='data/', train=False, download=True)
 
-
+def sort_testset():
+	result = {1: [0, []], 2: [0, []], 3: [0, []], 4: [0, []], 5: [0, []], 6: [0, []], 7: [0, []], 8: [0, []], 9: [0, []], 0: [0, []]}
+	for i, number in enumerate(testset):
+		result[number[1]][1].append(i)
+	# for number in result.keys():
+	# 	print(number, len(result[number][1]))
+	return result
 
 def prolog_format_sudoku(sudoku_size, filename, n_remove):
+	test_dict = sort_testset()
+	# print(test_dict[5])
 	if sudoku_size * sudoku_size < n_remove:
 		return 0 #can't remove more numbers then there are fields
 	new_filename = filename.split('.')[0][:-7] + '_' + str(n_remove) + 'open.pl'
@@ -11,11 +21,20 @@ def prolog_format_sudoku(sudoku_size, filename, n_remove):
 		with open(new_filename, 'w') as new_sudoku_file:
 			sudoku_file.readline() #skip header
 			for sudoku in sudoku_file.readlines():
+				print(sudoku)
 				i = 0
 				sudoku = sudoku[2:-3].split('],[')
 				new_sudoku = []
 				for row in sudoku:
-					new_sudoku += row.split(',')
+					for number in row.split(','):
+						number = int(number)
+						print(number)
+						try:
+							new_sudoku.append(test_dict[number][1][test_dict[number][0]])
+						except:
+							test_dict[number][0] = 0
+							new_sudoku.append(test_dict[number][1][test_dict[number][0]])
+						test_dict[number][0] += 1
 				sudoku = new_sudoku
 				while i < n_remove:
 					random_coor = (random.randint(1, sudoku_size*sudoku_size))
@@ -24,11 +43,9 @@ def prolog_format_sudoku(sudoku_size, filename, n_remove):
 						i += 1
 					else:
 						continue
-				# print(str(new_sudoku))
+				print(str(new_sudoku))
 				new_sudoku_file.write('sudoku(' + str(new_sudoku).replace('\'', '') + ',Solution).\n')
 				# return 0
-
-				# print(new_filename)
 
 
 
@@ -56,3 +73,4 @@ def prolog_format_sudoku(sudoku_size, filename, n_remove):
 
 
 prolog_format_sudoku(4, '4x4_sudokus_solved.txt', 5)
+
